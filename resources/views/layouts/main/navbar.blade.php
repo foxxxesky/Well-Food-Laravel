c2fcc4fb96856a31cccaaaa4d2ad0a50e06d6915
foxxxesky <abdurrahman1467@gmail.com>
2022-06-15 06:14:43 +0800

    adding jumbotron

---
+++ resources/views/layouts/main/navbar.blade.php
@@ -0,0 +1,4 @@
+<div class="text-white text-center jumbotron" style="background-image: url('images/img_jumbotron.png');">
+    <h1 class="heading_jumbotron">Well Food</h1>
+    <p class="text_jumbotron">You're What You Eat</p>
+</div>
