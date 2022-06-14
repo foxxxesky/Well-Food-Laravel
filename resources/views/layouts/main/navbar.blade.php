aeca4983dd7d8d8019179063fcc0929bc98d8bf8
foxxxesky <abdurrahman1467@gmail.com>
2022-06-15 06:29:26 +0800

    adding navbarr

--- resources/views/layouts/main/navbar.blade.php
+++ resources/views/layouts/main/navbar.blade.php
@@ -2,3 +2,26 @@
     <h1 class="heading_jumbotron">Well Food</h1>
     <p class="text_jumbotron">You're What You Eat</p>
 </div>
+
+<div class="container text-center p-5">
+    <div class="row">
+        <div class="col">
+            <a href="" class="btn btn-outline-secondary">High Protein</a>
+        </div>
+        <div class="col">
+            <a href="" class="btn btn-outline-secondary">Low-Carb</a>
+        </div>
+        <div class="col">
+            <a href="" class="btn btn-outline-secondary">Low-Fat</a>
+        </div>
+        <div class="col">
+            <a href="" class="btn btn-outline-secondary">Low-Sodium</a>
+        </div>
+        <div class="col">
+            <a href="" class="btn btn-outline-secondary">Low-Sugar</a>
+        </div>
+        <div class="col">
+            <a href="" class="btn btn-outline-secondary">Vegetarian</a>
+        </div>
+    </div>
+</div>
